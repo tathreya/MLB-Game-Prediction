297e157025f7e964785db89e05b7dc97e6ff6020
tathreya <tanay.athreya@gmail.com>
2025-06-07 16:42:56 -0700

    initial work on fetching current schedule

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 from src.teamsInitializer.initializeTeams import fetchMLBTeams
+
+from src.scheduleUpdater.fetchCurrentSchedule import fetchAndUpdateCurrentSchedule
 def main():
     print('here inside main')
     fetchMLBTeams()
+    fetchAndUpdateCurrentSchedule()
 
 if __name__ == "__main__":
     main()
--- src/scheduleUpdater/fetchCurrentSchedule.py
+++ src/scheduleUpdater/fetchCurrentSchedule.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 
 import os
 from dotenv import load_dotenv
@@ -16,34 +17,62 @@ def fetchAndUpdateCurrentSchedule():
         "gameType": "R",     # Regular season
     }
 
+    conn = sqlite3.connect("databases/MLB_Betting.db")
+    cursor = conn.cursor()
+
+    insert_statement = """
+        INSERT OR IGNORE INTO CurrentSchedule (
+            id, 
+            date, 
+            home_team
+            away_team,
+            status,
+            season,
+            season_type
+        ) VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
+
+
     try:
         response = requests.get(base_url + "schedule", params=params)
         data = response.json()
 
         # dictionary of form {key = date: value = games on that day}
-        schedule = {}
+        # schedule = {}
         
         i = 0
         # iterate through each day
         for day in data.get("dates", []):
 
-            # get the date
-            date = day["date"]
-            # get all the games on that day
-            games = day.get("games", [])
-            schedule[date] = games 
             # TODO: remove eventually
-            if (i == 2):
+            if (i == 1):
                 break
 
-        for game in schedule.get("2025-03-27"):
-            print(game)
+            # get the date
+            date = day["date"]
+            print(date)
+            # get all the games on that day
+            games = day.get("games", [])
+
+            for game in games:
+                print(game["gamePk"])
+                print(game["season"])
+                print(game["gameType"])
+                print(game["gameDate"])
+
+
+            i = i + 1
+
+            #schedule[date] = games 
+      
 
            
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred while fetching API data: {http_err}")  
     except requests.exceptions.RequestException as err:
-        print(f"Other error occurred while fetching API data: {err}")  
+        print(f"Other error occurred while fetching API data: {err}")
+    except Exception as e:
+        print(f"Other error occurred while fetching saving games to DB: {e}")  
 
   
     # TODO: if current date is past the last entry in schedule, fetch playoff shcedule instead
