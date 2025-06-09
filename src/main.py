12ddab97faa32bf685c08bd54b978c3c643102e5
tathreya <tanay.athreya@gmail.com>
2025-06-09 13:38:56 -0700

    rearranged folder structure and added debug logging

---
+++ src/main.py
@@ -0,0 +1,20 @@
+
+from teamsInitializer.initializeTeams import fetchMLBTeams
+from scheduleUpdater.fetchCurrentSchedule import fetchAndUpdateCurrentSchedule
+import logging
+
+# Set up basic configuration
+logging.basicConfig(
+    filename='app.log',
+    filemode='w', 
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def main():
+    print('here inside main')
+    fetchMLBTeams()
+    #fetchAndUpdateCurrentSchedule()
+
+if __name__ == "__main__":
+    main()
