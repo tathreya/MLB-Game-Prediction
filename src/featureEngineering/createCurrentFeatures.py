37258dffb9cb482aa54d798da7bc4795437ea41c
tathreya <tanay.athreya@gmail.com>
2025-07-21 20:58:01 -0700

    more scaffolding

--- src/featureEngineering/createCurrentFeatures.py
+++ src/featureEngineering/createCurrentFeatures.py
@@ -1,5 +1,2 @@
 # TODO: write this function to feature engineer (add features to Features table) for the current season 
-# up until current day
-
-# use same logic as historical features but only do it on games before current day so have a WHERE clause on the
-# currentSchedule table
+# up until current day --> JUST DO IT IN THE CREATE HISTORICAL FEATURES FILE AND HAVE SPECIAL CASE FOR THE CURRENT SEASON!
